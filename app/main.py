a087ec853c956e16543c08bac40a44e9366303f8
origox <jfrandberg@gmail.com>
2022-09-25 12:30:43 +0200

    initial items api added

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+from .api import items
+
+app = FastAPI()
+
+app.include_router(items.router)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Bigger Applications!"}
