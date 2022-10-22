ab50c64ebfd882081727287a0cd2957cb102b0bf
origox <jfrandberg@gmail.com>
2022-10-22 09:53:10 +0200

    unit test added

---
+++ tests/test_main.py
@@ -0,0 +1,13 @@
+import pytest
+from httpx import AsyncClient
+
+from app.main import app
+
+
+@pytest.mark.anyio
+async def test_root():
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello CI/CD powered Application!.!"}
+
